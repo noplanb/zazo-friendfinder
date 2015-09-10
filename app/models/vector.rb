7acdac39bda37d5260af906660361194df0cb36a
Ivan Kornilov <vano468@gmail.com>
2015-09-10 16:40:34 +0300

    Implement vector validations with specs

--- app/models/vector.rb
+++ app/models/vector.rb
@@ -1,12 +1,44 @@
 class Vector < ActiveRecord::Base
+  ALLOWED_NAMES = %w(mobile email facebook).freeze
+  ALLOWED_ADDITIONS = {
+    mobile:   %w(sms_messages_sent marked_as_favorite),
+    email:    %w(email_messages_sent),
+    facebook: %w()
+  }.stringify_keys.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   belongs_to :contact
 
   validates :contact, :name, :value, presence: true
+  validates :name, inclusion: { in: ALLOWED_NAMES, message: '%{value} is not a allowed vector name' }
+  validate :additions_must_be_allowed, :value_has_correct_format, unless: 'name.nil?'
 
   scope :mobile, -> { where name: 'mobile' }
   scope :email,  -> { where name: 'email' }
 
   def additions_value(key, default = nil)
     (additions.try :[], key) || default
+  end
+
+  private
+
+  #
+  # validations
+  #
+
+  def additions_must_be_allowed
+    additions && additions.keys.each do |key|
+      errors.add(:additions, "'#{key}' is not allowed condition for '#{name}' vector") unless ALLOWED_ADDITIONS[name].include? key
+    end
+  end
+
+  def value_has_correct_format
+    message = "'#{value}' has incorrect format for '#{name}' vector"
+    case name
+      when 'mobile'
+        errors.add(:value, message) if Normalize.mobile(value).nil?
+      when 'email', 'facebook'
+        errors.add(:value, message) if (value =~ VALID_EMAIL_REGEX).nil?
+    end
   end
 end
--- spec/models/vector_spec.rb
+++ spec/models/vector_spec.rb
@@ -5,5 +5,82 @@ RSpec.describe Vector, type: :model do
     it { is_expected.to validate_presence_of :contact }
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :value }
+
+    describe '#additions_must_be_allowed' do
+      before { vector.valid? }
+
+      context 'valid' do
+        context 'mobile' do
+          let(:vector) { FactoryGirl.build :vector_mobile, additions: { sms_messages_sent: 15 } }
+
+          it { expect(vector).to be_valid }
+        end
+
+        context 'email' do
+          let(:vector) { FactoryGirl.build :vector_email, additions: { email_messages_sent: 65 } }
+
+          it { expect(vector).to be_valid }
+        end
+
+        context 'facebook' do
+          let(:vector) { FactoryGirl.build :vector_facebook }
+
+          it { expect(vector).to be_valid }
+        end
+      end
+
+      context 'invalid' do
+        context 'mobile' do
+          let(:vector) { FactoryGirl.build :vector_mobile, additions: { email_messages_sent: 15 } }
+
+          it { expect(vector).to be_invalid }
+          it { expect(vector.errors.messages).to eq({ additions: ['\'email_messages_sent\' is not allowed condition for \'mobile\' vector']}) }
+        end
+
+        context 'email' do
+          let(:vector) { FactoryGirl.build :vector_email, additions: { sms_messages_sent: 65 } }
+
+          it { expect(vector).to be_invalid }
+          it { expect(vector.errors.messages).to eq({ additions: ['\'sms_messages_sent\' is not allowed condition for \'email\' vector']}) }
+        end
+
+        context 'facebook' do
+          let(:vector) { FactoryGirl.build :vector_facebook, additions: { marked_as_favorite: true } }
+
+          it { expect(vector).to be_invalid }
+          it { expect(vector.errors.messages).to eq({ additions: ['\'marked_as_favorite\' is not allowed condition for \'facebook\' vector']}) }
+        end
+      end
+    end
+
+    describe '#value_has_correct_format' do
+      before { vector.valid? }
+
+      context 'valid mobile' do
+        let(:vector) { FactoryGirl.build :vector_mobile }
+
+        it { expect(vector).to be_valid }
+      end
+
+      context 'invalid mobile' do
+        let(:vector) { FactoryGirl.build :vector_mobile, value: 'xxxxxxxxx' }
+
+        it { expect(vector).to be_invalid }
+        it { expect(vector.errors.messages).to eq({ value: ['\'xxxxxxxxx\' has incorrect format for \'mobile\' vector']}) }
+      end
+
+      context 'valid email' do
+        let(:vector) { FactoryGirl.build :vector_email }
+
+        it { expect(vector).to be_valid }
+      end
+
+      context 'invalid email' do
+        let(:vector) { FactoryGirl.build :vector_email, value: 'asdasd@asdasd' }
+
+        it { expect(vector).to be_invalid }
+        it { expect(vector.errors.messages).to eq({ value: ['\'asdasd@asdasd\' has incorrect format for \'email\' vector']}) }
+      end
+    end
   end
 end
