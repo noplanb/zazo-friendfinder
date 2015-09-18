cc3cd30d8cbb823bf759f4674d4bfdfef2cd084a
Ivan Kornilov <vano468@gmail.com>
2015-09-18 15:24:45 +0300

    Add some controllers specs

--- spec/controllers/api/v1/suggestions_controller_spec.rb
+++ spec/controllers/api/v1/suggestions_controller_spec.rb
@@ -5,9 +5,38 @@ RSpec.describe Api::V1::SuggestionsController, type: :controller do
   let(:user_mkey) { '7qdanSEmctZ2jPnYA0a1' }
   let(:user_auth) { 'yLPv2hZ4DPRq1wGlQvqm' }
 
-  describe 'POST #create' do
+  describe 'GET #index' do
+    let!(:contact_1) { FactoryGirl.create :contact, owner: user_mkey }
+    let!(:contact_2) { FactoryGirl.create :contact, owner: user_mkey }
     before do
       authenticate_with_http_digest(user_mkey, user_auth) { get :index, format: :json }
+    end
+
+    it { expect(response).to be_success }
+  end
+
+  describe 'POST #reject' do
+    let(:contact_1) { FactoryGirl.create :contact, owner: user_mkey }
+    let(:contact_2) { FactoryGirl.create :contact, owner: user_mkey }
+    let(:params) do
+      { rejected: [contact_1.id, contact_2.id] }
+    end
+    before do
+      authenticate_with_http_digest(user_mkey, user_auth) { post :reject, params.merge({format: :json}) }
+    end
+
+    it { expect(response).to be_success }
+  end
+
+  describe 'POST #recommend' do
+    let(:contact) { FactoryGirl.create :contact, owner: FactoryGirl.build(:user).mkey }
+    let(:params) do
+      { recommendations: {
+          contact_mkey: FactoryGirl.build(:user).mkey,
+          to_mkeys: [contact.owner] } }
+    end
+    before do
+      authenticate_with_http_digest(user_mkey, user_auth) { post :recommend, params.merge({format: :json}) }
     end
 
     it { expect(response).to be_success }
