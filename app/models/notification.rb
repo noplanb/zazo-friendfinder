84cd58c4e27e50ef967be4e43f02890aabf1bae7
Ivan Kornilov <vano468@gmail.com>
2015-12-07 16:31:24 +0300

    Implement status bar on dashboard page

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -16,6 +16,7 @@ class Notification < ActiveRecord::Base
 
   scope :unsubscribed_by_contacts, -> (contacts) { where(status: 'unsubscribed', contact: contacts) }
   scope :by_owner_mkey, -> (owner_mkey) { Notification.includes(:contact).where(contacts: { owner_mkey: owner_mkey }) }
+  scope :by_state, -> (state) { Notification.where state: state }
 
   def self.match_by_contact?(contact)
     !where(contact: contact).empty?
