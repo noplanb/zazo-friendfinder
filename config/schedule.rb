852c096d31ae79b4cede18d26546015a47dd06d4
Ivan Kornilov <vano468@gmail.com>
2016-03-03 18:05:35 +0300

    Fix cron env vars

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ set :output, '/usr/src/app/log/cron.log'
 [
   :PATH, :GEM_HOME, :RACK_ENV, :RAILS_ENV, :secret_key_base,
   :db_name, :db_host, :db_port, :db_username, :db_password,
-  :statistics_api_base_url, :events_api_base_url,
+  :dataprovider_api_base_url, :dataprovider_api_token,
   :rollbar_access_token, :newrelic_license_key,
   :redis_host, :redis_port,
 ].each { |key| env key, ENV[key.to_s] }
