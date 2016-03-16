7d1ac35be7444249ec4f75d3b7503fe73b648ac4
Ivan Kornilov <vano468@gmail.com>
2016-03-16 23:10:05 +0300

    Fix specs, fix notifications table view

--- spec/models/notification/email_data_spec.rb
+++ spec/models/notification/email_data_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Notification::EmailData, type: :model do
 
     it do
       is_expected.to eq to: 'vano468@gmail.com',
-                        from: 'support@zazoapp.com',
+                        #from: 'support@zazoapp.com',
                         subject: "#{contact.display_name} joined Zazo!",
                         body: 'Hello from Russia!'
     end
