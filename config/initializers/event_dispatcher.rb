5bb0434c3b4e4acdf4188ff1beed294aba5c5491
Ivan Kornilov <vano468@gmail.com>
2016-04-11 16:53:19 +0300

    Add commented code for testing events dispatching

--- config/initializers/event_dispatcher.rb
+++ config/initializers/event_dispatcher.rb
@@ -3,3 +3,21 @@ Zazo::Tools::EventDispatcher.configure do |config|
   config.queue_url = Figaro.env.sqs_queue_url
   config.logger = Rails.logger
 end
+
+#
+# code below is just for testing
+#
+
+=begin
+module FooModule
+  def self.included base
+    base.instance_eval do
+      def emit(name, params = {})
+        Rails.logger.info ['EventDispatcher', name, params]
+      end
+    end
+  end
+end
+
+Zazo::Tools::EventDispatcher.send(:include, FooModule)
+=end
