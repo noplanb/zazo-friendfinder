79d66ff64ff9495a0b59433454c279d8c81d4452
Ivan Kornilov <vano468@gmail.com>
2016-04-11 15:05:28 +0300

    Add dispatching for opened email event

--- app/services/web_client/track_email.rb
+++ app/services/web_client/track_email.rb
@@ -7,6 +7,12 @@ class WebClient::TrackEmail
 
   def do
     return unless notification
+    Zazo::Tools::EventDispatcher.emit(%w(email opened),
+      triggered_by: 'ff:notification',
+      initiator: 'notification',
+      initiator_id: notification.nkey,
+      target: 'owner',
+      target_id: notification.contact.owner.mkey)
     WriteLog.info(self, "tracked; owner: #{notification.contact.owner.mkey}")
   end
 end
