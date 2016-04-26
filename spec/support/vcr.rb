666ce872afdc4ac883a1620a2ba4807a8d710199
Ivan Kornilov <vano468@gmail.com>
2016-04-26 16:49:37 +0300

    Fix bug with VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -14,6 +14,11 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  c.before_record do |i|
+    i.response.headers.delete('Set-Cookie')
+    i.request.headers.delete('Authorization')
+  end
 end
 
 def api_base_urls
