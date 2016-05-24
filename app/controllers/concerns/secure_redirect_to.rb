9f5b31c16e25c90e3a6f17762d61436b05feeacf
Ivan Kornilov <vano468@gmail.com>
2016-05-24 14:05:20 +0300

    Fix SecureRedirectTo module

--- app/controllers/concerns/secure_redirect_to.rb
+++ app/controllers/concerns/secure_redirect_to.rb
@@ -1,7 +1,7 @@
 module SecureRedirectTo
   def secure_redirect_to(relative_path, options = {})
     if %w(production staging).include?(Rails.env)
-      redirect_to("https://#{request.domain}#{relative_path}", options)
+      redirect_to("https://#{request.host}#{relative_path}", options)
     else
       redirect_to(relative_path, options)
     end
