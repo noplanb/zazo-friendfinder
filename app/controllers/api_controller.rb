31b751721b18697b0f0d4d297a2b4cf6cfb47d8f
Ivan Kornilov <vano468@gmail.com>
2016-05-27 16:06:13 +0300

    Implement Controllers::HandleApiInteractor service

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,5 @@
 class ApiController < ApplicationController
   include Authentication
-  include HandleInteractor
 
   def handle_with_manager(manager)
     if manager.do
@@ -10,6 +9,14 @@ class ApiController < ApplicationController
     else
       manager.log_messages(:failure)
       render status: :unprocessable_entity, json: { status: :failure, errors: manager.errors }
+    end
+  end
+
+  def handle_interactor(type_settings, interactor, &callback)
+    Controllers::HandleApiInteractor.new(
+      context: self, interactor: interactor,
+      type_settings: type_settings, callback: callback).call do |handler|
+      handler.render? ? render(handler.response) : handler.result
     end
   end
 end
---
+++ app/services/controllers/handle_api_interactor.rb
@@ -0,0 +1,75 @@
+class Controllers::HandleApiInteractor
+  attr_reader :context, :interactor, :type, :settings, :callback,
+              :response, :result
+
+  def initialize(context:, interactor:, type_settings:, callback: nil)
+    @context    = context
+    @interactor = interactor
+    @callback   = callback
+
+    @type, @settings = Array.wrap(type_settings)
+    @settings = default_settings(type).merge(@settings || {})
+  end
+
+  def call
+    if interactor.valid?
+      prepare_data_by_success_case
+      log_data_by_case(:success) if settings[:logging_success]
+      callback.call if callback
+    else
+      prepare_data_by_failure_case
+      log_data_by_case(:failure) if settings[:logging_failure]
+    end
+    yield(self)
+  end
+
+  def render?
+    !!response
+  end
+
+  private
+
+  def prepare_data_by_success_case
+    case type
+      when :render
+        @response = { status: :success }
+        @response = response.merge(data: interactor.result) if settings[:result]
+        @response = { json: response }
+      when :result
+        @result = interactor.result
+      else nil
+    end
+  end
+
+  def prepare_data_by_failure_case
+    @response = {
+      status: :unprocessable_entity,
+      json: { status: :failure, errors: interactor.errors.messages }
+    }
+  end
+
+  def log_data_by_case(status)
+    _current_user = "current_user: #{context.current_user};"
+    _response     = "response: #{response.to_json};"
+    _inputs       = "inputs: #{interactor.inputs.to_json};"
+    _errors       = "errors: #{interactor.errors.messages.to_json};"
+
+    if status == :success
+      Zazo::Tools::Logger.info(context, "#{status}; #{_current_user} #{_inputs} #{_response}")
+    else
+      Zazo::Tools::Logger.info(context, "#{status}; #{_current_user} #{_inputs} #{_response} #{_errors}")
+    end
+  end
+
+  def default_settings(type)
+    case type
+      when :render
+        { result: true,
+          logging_success: true,
+          logging_failure: true }
+      when :result
+        { logging_failure: true }
+      else {}
+    end
+  end
+end
