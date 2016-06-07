48f31a82d2ff37a208a6b69a0b6660c719dadd6b
Ivan Kornilov <vano468@gmail.com>
2016-06-07 18:30:42 +0300

    Update zazo_tools gem with new http logger

--- config/initializers/zazo_tools.rb
+++ config/initializers/zazo_tools.rb
@@ -1,3 +1,15 @@
+Zazo::Tools::Logger.configure do |config|
+  logstash_url = Figaro.env.logstash_url
+  if logstash_url && !(Rails.env.test?)# || Rails.env.development?)
+    config.logstash_enabled = true
+    config.logstash_host = logstash_url.split(':').first
+    config.logstash_port = logstash_url.split(':').last
+    config.logstash_username = Figaro.env.logstash_username
+    config.logstash_password = Figaro.env.logstash_password
+  end
+  config.project_name = AppConfig.app_name_key
+end
+
 Zazo::Tools::EventDispatcher.configure do |config|
   config.send_message_enabled = false if Rails.env.test? || Rails.env.development?
   config.queue_url = Figaro.env.sqs_queue_url
