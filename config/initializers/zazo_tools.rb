24fd92e6218b3dc3e97018dae7246bd9525d3245
Ivan Kornilov <vano468@gmail.com>
2016-06-07 18:38:18 +0300

    Fix `Zazo::Tools::Logger` configuration

--- config/initializers/zazo_tools.rb
+++ config/initializers/zazo_tools.rb
@@ -1,6 +1,6 @@
 Zazo::Tools::Logger.configure do |config|
   logstash_url = Figaro.env.logstash_url
-  if logstash_url && !(Rails.env.test?)# || Rails.env.development?)
+  if logstash_url && !(Rails.env.test? || Rails.env.development?)
     config.logstash_enabled = true
     config.logstash_host = logstash_url.split(':').first
     config.logstash_port = logstash_url.split(':').last
