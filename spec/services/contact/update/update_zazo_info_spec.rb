12288c70d6d7821bbc18956df829cd0bcc90a6cb
Ivan Kornilov <vano468@gmail.com>
2016-08-09 18:42:19 +0300

    Fix specs for Contact::Update::UpdateZazoInfo service

--- spec/services/contact/update/update_zazo_info_spec.rb
+++ spec/services/contact/update/update_zazo_info_spec.rb
@@ -2,27 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Contact::Update::UpdateZazoInfo do
   let(:contact) { FactoryGirl.create(:contact, owner_mkey: owner_mkey, zazo_mkey: zazo_mkey) }
-  let(:instance) { described_class.new contact }
+  let(:instance) { described_class.new(contact) }
   let(:owner_mkey) { 'xxxxxxxxxxxx' }
   let(:zazo_mkey) { 'GBAHb0482YxlJ0kYwbIS' }
 
-  describe '#do' do
-    subject { contact.reload }
-    before { instance.do }
+  describe '#do', vcr: { cassette: 'not_marked_as_friend' } do
+    before do
+      instance.do
+      contact.reload
+    end
 
-    it { expect(subject.first_name).to eq 'Ivan' }
-    it { expect(subject.last_name).to eq 'Kornilov' }
-    it { expect(subject.zazo_id).to eq 3686 }
-    it { expect(subject.zazo_mkey).to eq zazo_mkey }
+    it { expect(contact.first_name).to eq('Ivan') }
+    it { expect(contact.last_name).to eq('Kornilov') }
+    it { expect(contact.zazo_id).to eq(3686) }
+    it { expect(contact.zazo_mkey).to eq(zazo_mkey) }
 
     describe 'additions[marked_as_friend]' do
-      context 'true' do
+      context 'true', vcr: { cassette: 'marked_as_friend' } do
         let(:owner_mkey) { '7qdanSEmctZ2jPnYA0a1' }
-        it { expect(subject.additions['marked_as_friend']).to eq true }
+
+        it { expect(contact.additions['marked_as_friend']).to eq(true) }
       end
 
       context 'false' do
-        it { expect(subject.additions['marked_as_friend']).to eq false }
+        it { expect(contact.additions['marked_as_friend']).to eq(false) }
       end
     end
   end
