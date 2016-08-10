def861faf4867e1a1e7f655c9b023045d110009e
Ivan Kornilov <vano468@gmail.com>
2016-08-10 11:42:42 +0300

    Fix rest specs

--- spec/services/notification/send_spec.rb
+++ spec/services/notification/send_spec.rb
@@ -13,33 +13,29 @@ RSpec.describe Notification::Send do
 
     context 'when email notification' do
       let(:kind) { 'email' }
+
       subject { notification.reload.state }
 
-      context 'when contact owner is existing zazo user with persisted emails' do
+      context 'when contact owner is existing zazo user with persisted emails',
+        vcr: { cassette: 'email_when_owner_has_emails' } do
         let(:contact) { create(:contact, owner_mkey: 'GBAHb0482YxlJ0kYwbIS') }
 
-        it { is_expected.to eq 'sent' }
+        it { is_expected.to eq('sent') }
       end
 
-      context 'when contact owner is existing zazo user without persisted emails' do
+      context 'when contact owner is existing zazo user without persisted emails',
+        vcr: { cassette: 'email_when_owner_has_not_emails' } do
         let(:contact) { create(:contact, owner_mkey: 'XqUn9Fs5YHd75l1rin76') }
 
-        it { is_expected.to eq 'canceled' }
+        it { is_expected.to eq('canceled') }
       end
 
-      context 'when contact owner is not exist' do
+      context 'when contact owner is not exist',
+        vcr: { cassette: 'email_when_owner_is_not_exist' } do
         let(:contact) { create(:contact, owner_mkey: 'xxxxxxxxxxxx') }
 
-        it { is_expected.to eq 'canceled' }
+        it { is_expected.to eq('canceled') }
       end
-
-=begin
-      context 'when email is not correct' do
-        let(:contact) { create(:contact, owner_mkey: 'dz4X0EvprPJO6fGysT8X') }
-
-        it { is_expected.to eq 'error' }
-      end
-=end
     end
 
     #
@@ -50,14 +46,11 @@ RSpec.describe Notification::Send do
       let(:kind) { 'mobile' }
       subject { notification.reload.state }
 
-      context 'when contact owner is existing zazo push_user' do
+      context 'when contact owner is existing zazo push_user',
+        vcr: { cassette: 'mobile_when_owner_has_push_user' } do
         let(:contact) { create(:contact, owner_mkey: 'GBAHb0482YxlJ0kYwbIS') }
 
-        it { is_expected.to eq 'sent' }
-      end
-
-      context 'when contact owner is not existing zazo push_user' do
-        it
+        it { is_expected.to eq('sent') }
       end
     end
   end
